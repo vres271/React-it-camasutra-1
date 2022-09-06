c84eeec4b533f23d44218b511524d7d61c6708d4
vres271 <vres271@gmail.com>
2022-09-06 09:00:40 +0400

    09

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+const Header = ()=>{
+  return (
+    <div>
+      <h1>This is my header 1</h1>
+      <a href="#1">Home</a>
+      <a href="#2">News Feed</a>
+      <a href="#3">Messages</a>
+    </div>
+    )
+}
+
+export default Header;
+
+
+
+
